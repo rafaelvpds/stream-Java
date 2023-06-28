54e8d0c65339e9fed8e4a106e19967d706a4553b
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-06-28 17:28:53 -0300

    stream

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,7 +34,7 @@ public class Program {
 
 		System.out.println();
 
-		System.out.println("Sequecia de fibonate");
+		System.out.println("Sequecia de fibonate!");
 
 		Stream<Long> st4 = Stream.iterate(new Long[] { 0L, 1L }, p -> new Long[] { p[1], p[0] + p[1] }).map(p -> p[0]);
 		System.out.println(Arrays.toString(st4.limit(10).toArray()));
